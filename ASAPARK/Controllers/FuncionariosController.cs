5d0b61ee9b8af18039c79187694348c3a827fc22
Nilson Alcides <nilson_alcides@hotmail.com.com>
2022-10-31 14:18:38 -0300

    Add String de conexao + Controller

---
+++ ASAPARK/Controllers/FuncionariosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASAPARK.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        // GET: Funcionarios
+        public ActionResult Funcionarios()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASAPARK/Controllers/VendasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASAPARK.Controllers
+{
+    public class VendasController : Controller
+    {
+        // GET: Vendas
+        public ActionResult RealizarVenda()
+        {
+            return View();
+        }
+
+        public ActionResult Consultar()
+        {
+            return View();
+        }
+    }
+}
