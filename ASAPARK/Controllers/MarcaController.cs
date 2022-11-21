374bfc02e54725c6b122cca474df4f555c5633a6
Nilson Alcides <nilson_alcides@hotmail.com.com>
2022-11-21 14:31:32 -0300

    Consulta e cadastro de marca

---
+++ ASAPARK/Controllers/MarcaController.cs
@@ -0,0 +1,43 @@
+﻿using Negocios;
+using ObjetoTransferencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASAPARK.Controllers
+{
+    public class MarcaController : Controller
+    {
+        MarcaNegocios marcaNegocios = new MarcaNegocios();
+        // GET: Marca
+        //Listar Pessoa fisica
+        public ActionResult CadMarca()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadMarca(Marca marca)
+        {
+            //string recebeOpcao = "";
+
+            if (ModelState.IsValid)
+            {
+                marcaNegocios.Inserir(marca);
+                //TODO Imprementar redirecionamento diferenetes
+
+                
+
+            }
+            return RedirectToAction(nameof(ConsultarMarca));
+
+
+        }
+        public ActionResult ConsultarMarca()
+        {
+            return View(marcaNegocios.carregarMarcaGrid());
+        }
+    }
+}
