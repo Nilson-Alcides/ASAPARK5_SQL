95fd0d3995d631c2c0ecd1a094679018576dab6d
Nilson Alcides <nilson_alcides@hotmail.com.com>
2022-11-21 16:19:03 -0300

    Atualizar, excluir marca

--- ASAPARK/Controllers/MarcaController.cs
+++ ASAPARK/Controllers/MarcaController.cs
@@ -11,6 +11,7 @@ namespace ASAPARK.Controllers
     public class MarcaController : Controller
     {
         MarcaNegocios marcaNegocios = new MarcaNegocios();
+        Marca marca = new Marca();
         // GET: Marca
         //Listar Pessoa fisica
         public ActionResult CadMarca()
@@ -27,17 +28,51 @@ namespace ASAPARK.Controllers
             {
                 marcaNegocios.Inserir(marca);
                 //TODO Imprementar redirecionamento diferenetes
-
-                
-
             }
             return RedirectToAction(nameof(ConsultarMarca));
 
 
         }
+        // Cosutar todas Marcas
         public ActionResult ConsultarMarca()
         {
             return View(marcaNegocios.carregarMarcaGrid());
         }
+
+        //Detalhes da Marca
+        public ActionResult MarcaDetalhes(int id)
+        {
+            return View(marcaNegocios.carregarMarcaGrid().Find(marca => marca.IdMarca == id));
+        }
+        // Editar Marca
+        public ActionResult EditarMarca(int id)
+        {
+            //if (Session["usuariologado"] == null || Session["senhaLogado"] == null)
+            //{
+            //    return RedirectToAction("Index", "Home");
+            //}
+            //else
+            //{
+                return View(marcaNegocios.carregarMarcaGrid().Find(marca => marca.IdMarca == id));
+            //}
+        }
+        // EDITAR Marca
+        [HttpPost]
+        public ActionResult EditarMarca(Marca marca)
+        {
+            marcaNegocios.AlterarMarca(marca);
+            return RedirectToAction(nameof(ConsultarMarca));
+        }
+        // EXCLUIR Marca
+        public ActionResult ExcluirMarca(int id)
+        {
+            Marca marca = new Marca();
+            marca.IdMarca = id;
+            string retorno = marcaNegocios.Excluir(marca);
+
+            return RedirectToAction(nameof(ConsultarMarca));
+        }
+
+
     }
 }
